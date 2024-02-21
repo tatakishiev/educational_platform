bf83d1d5a64e3277b46812df4654cf4c736cb096
timur.atakishiev <timur.atakishiev@beamtrail.com>
2024-02-21 17:31:57 +0400

    Rename table

--- priv/repo/migrations/20240221162909_class_to_lesson_many_to_many.exs
+++ priv/repo/migrations/20240221162909_class_to_lesson_many_to_many.exs
@@ -2,7 +2,7 @@ defmodule EducationalPlatform.Repo.Migrations.ClassToLessonManyToMany do
   use Ecto.Migration
 
   def change do
-    create table(:classes_lesson) do
+    create table(:classes_lessons) do
       add :class_id, references(:class)
       add :lesson_id, references(:lesson)
     end
